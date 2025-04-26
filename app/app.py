0b0f74f1525165014c28dd7986dee47b1804a484
vxnnio <11146027@ntub.edu.tw>
2025-04-27 00:11:49 +0800

    評估表單頁面

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ from app.routes.auth import auth_bp
 from app.routes.admin import admin_bp
 from app.routes.main import main_bp
 from app.routes.training import training_bp
+from app.routes.evaluation import evaluation_bp
 from app.models import User
 from app.models.announcement import Announcement
 from dotenv import load_dotenv
@@ -29,6 +30,7 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(training_bp)
+    app.register_blueprint(evaluation_bp, url_prefix='/evaluation')
     
     
     @login_manager.user_loader
--- app/database.py
+++ app/database.py
@@ -3,24 +3,24 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import Config
 from contextlib import contextmanager
-from sqlalchemy.ext.declarative import declarative_base
+
+# 定義基本模型
 Base = declarative_base()
 
-
-
+# 設定資料庫連線URL
 DATABASE_URL = f"mysql+pymysql://{Config.DB_USERNAME}:{Config.DB_PASSWORD}@{Config.DB_HOST}/{Config.DB_NAME}"
 
+# 建立資料庫引擎
 engine = create_engine(DATABASE_URL, echo=False)
 
+# 創建 sessionmaker，用於建立 session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
-
+# 會話管理器
 @contextmanager
 def get_db():
     db = SessionLocal()
     try:
-        yield db
+        yield db  # 這裡會返回 db 會話給呼叫者
     finally:
-        db.close()
+        db.close()  # 使用後自動關閉會話
