e0694a0873cd76741b0b34bc834020914f73fff9
vxnnio <11146027@ntub.edu.tw>
2025-05-22 23:10:32 +0800

    個人資料有身高體重

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import relationship
 from app.database import Base
-
 
 class User(Base):
     __tablename__ = "user"
@@ -10,21 +9,30 @@ class User(Base):
     username = Column(String(80), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
     role = Column(String(20), nullable=False)
+
+    full_name = Column(String(100), default="")
+    birth_date = Column(Date, nullable=True)
+    gender = Column(String(10), default="")  # 建議用 '男', '女', '其他'
+    height = Column(Integer, nullable=True)  # 單位：公分
+    weight = Column(Integer, nullable=True)  # 單位：公斤
+    region = Column(String(100), default="")
+    
     bio = Column(String(500), default="")
     profile_pic = Column(String(255), default="static/uploads/default.png")
 
     announcements = relationship("Announcement", back_populates="coach")
     trainings = relationship("Training", back_populates="user")
     evaluations = relationship("Evaluation", back_populates="user")
+
     # Flask-Login 需要的使用者方法
     def is_active(self):
-        return True  # 默認所有使用者都是活躍的
+        return True
 
     def is_authenticated(self):
-        return True  # 可以根據需要調整
+        return True
 
     def is_anonymous(self):
-        return False  # 可以根據需要調整
+        return False
 
     def get_id(self):
-        return str(self.id)  # Flask-Login 用於識別使用者的 ID
+        return str(self.id)
